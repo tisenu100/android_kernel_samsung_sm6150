6cc13fcc8d3a3f999a521b5c623d1fa86f4e8019
tisenu100 <tisenu100@github.com>
2024-10-10 18:33:08 +0300

    Add NobleROM's cmdline patcher with out own tastes

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -4,9 +4,18 @@
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
+#ifdef CONFIG_BOOTLOADER_ARGUMENTS_SPOOF
+#include <asm/setup.h>
+static char new_command_line[COMMAND_LINE_SIZE];
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", saved_command_line);
+#ifdef CONFIG_BOOTLOADER_ARGUMENTS_SPOOF
+        seq_printf(m, "%s\n", new_command_line);
+#else
+        seq_printf(m, "%s\n", saved_command_line);
+#endif
 	return 0;
 }
 
@@ -14,6 +23,38 @@ static int cmdline_proc_open(struct inode *inode, struct file *file)
 {
 	return single_open(file, cmdline_proc_show, NULL);
 }
+
+#ifdef CONFIG_BOOTLOADER_ARGUMENTS_SPOOF
+static void patch_flag(char *cmd, const char *flag, const char *val)
+{
+	size_t flag_len, val_len;
+	char *start, *end;
+
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
+
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
+}
+
+static void patch_bootloader_arguments(char *cmd)
+{
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
+	patch_flag(cmd, "androidboot.vbmeta.device_state=", "locked");
+	patch_flag(cmd, "androidboot.warranty_bit=", "0");
+	
+#ifdef CONFIG_D1Q_SPOOF
+        patch_flag(cmd, "androidboot.em.model=", "SM-S911B ");
+	patch_flag(cmd, "androidboot.bootloader=", "S911BXXS6CXHA");
+	patch_flag(cmd, "androidboot.product.model=", "SM-S911B ");
+#endif
+}
+#endif
 
 static const struct file_operations cmdline_proc_fops = {
 	.open		= cmdline_proc_open,
@@ -25,6 +66,10 @@ static const struct file_operations cmdline_proc_fops = {
 static int __init proc_cmdline_init(void)
 {
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
+#ifdef CONFIG_BOOTLOADER_ARGUMENTS_SPOOF
+        strcpy(new_command_line, saved_command_line);
+        patch_bootloader_arguments(new_command_line);
+#endif
 	return 0;
 }
 fs_initcall(proc_cmdline_init);
