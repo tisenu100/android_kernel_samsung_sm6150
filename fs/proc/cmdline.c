4d6cc72698d4b2bb08b5a16b2610325c86ebff70
tisenu100 <tisenu100@github.com>
2025-01-02 13:08:32 +0200

    Revert back to S24 China

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -49,9 +49,9 @@ static void patch_bootloader_arguments(char *cmd)
 	patch_flag(cmd, "androidboot.warranty_bit=", "0");
 	
 #ifdef CONFIG_D1Q_SPOOF
-        patch_flag(cmd, "androidboot.em.model=", "SM-S928B ");
-	patch_flag(cmd, "androidboot.bootloader=", "S928BXXS4AXKA");
-	patch_flag(cmd, "androidboot.product.model=", "SM-S928B ");
+        patch_flag(cmd, "androidboot.em.model=", "SM-S9210 ");
+	patch_flag(cmd, "androidboot.bootloader=", "S9210ZHS4AXK6");
+	patch_flag(cmd, "androidboot.product.model=", "SM-S9210 ");
 #endif
 }
 #endif
